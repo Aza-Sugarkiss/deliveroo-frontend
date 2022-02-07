f5a6ef5b885d9992bd1dcc260a0e4cfba196c6bf
aza <aza_85@protonmail.com>
2022-02-07 23:13:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Basket from "./components/Basket";
 
 function App() {
-  return (
+  const [data, setData] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://exo-deliveroo-backend.herokuapp.com/"
+        );
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement...</span>
+  ) : (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      <Header data={data.restaurant} />
+      <div className="menu container">
+        <div className="categories">
+          {data.categories.map((categories, index) => {
+            return (
+              categories.meals.length !== 0 && (
+                <Categories key={index} categories={categories} />
+              )
+            );
+          })}
+        </div>
+        <Basket />
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import logo from "../assets/img/logo.jpg";
+import "./header.css";
+
+const Header = ({ data }) => {
+  return (
+    <header>
+      <div className="container">
+        <img className="logo" src={logo} alt="logo-deliveroo" />
+      </div>
+      <div className="line"></div>
+      <div className="restaurant container">
+        <div className="description">
+          <h1>{data.name}</h1>
+          <p>{data.description}</p>
+        </div>
+        <img src={data.picture} alt="restaurant" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
